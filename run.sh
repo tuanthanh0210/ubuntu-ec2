439d5b357a2759dd3a3ad5401bdc955ceb4d72a1
Thanh Le Tuan <54875279+tuanthanh021094@users.noreply.github.com>
2023-02-19 00:49:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@ sudo apt update
 sudo apt install git
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt install nodejs
 sudo apt install npm
-npm install -g n
-npm install -g pm2
+sudo npm install -g n
+sudo npm install -g pm2
 sudo apt install make
 sudo apt install htop
 sudo snap install docker
