e2e339072c5cc055ec2ca90ef10f52b0e4abd26b
Thanh Le Tuan <54875279+tuanthanh021094@users.noreply.github.com>
2023-02-19 00:51:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 sudo apt update
 sudo apt install git
+echo "Install successfully : Git"
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt install nodejs
+echo "Install successfully : Node"
 sudo apt install npm
+echo "Install successfully : npm"
 sudo npm install -g n
 sudo npm install -g pm2
+echo "Install successfully : PM2"
 sudo apt install make
+echo "Install successfully : Makefile"
 sudo apt install htop
 sudo snap install docker
+echo "Install successfully : Docker"
 sudo npm i -g yarn
+echo "Install successfully : Yarn"
