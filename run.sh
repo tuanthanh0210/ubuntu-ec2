1f0224c86cea9c98bd6aae8ae1f9bd7508da9d8a
Thanh Le Tuan <54875279+tuanthanh021094@users.noreply.github.com>
2023-02-19 00:31:19 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install git
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt install nodejs
+sudo apt install npm
+npm install -g n
+npm install -g pm2
+sudo apt install make
+sudo apt install htop
+sudo snap install docker
+sudo npm i -g yarn
