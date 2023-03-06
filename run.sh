8bfd1d5f0621f1643a7c4e49c806e4f6e14dc214
Thanh Le Tuan <54875279+tuanthanh021094@users.noreply.github.com>
2023-03-07 01:34:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 sudo apt update
-sudo apt install git
+sudo apt install -y git
 echo "Install successfully : Git"
-sudo apt install nodejs
+sudo apt install -y nodejs
 echo "Install successfully : Node"
-sudo apt install npm
+sudo apt install -y npm
 echo "Install successfully : npm"
-sudo npm install -g pm2
+sudo npm install -y -g pm2
 echo "Install successfully : PM2"
-sudo npm install -g n
+sudo npm install -y -g n
 sudo n 18 && hash -r
 node -v
 echo "Install successfully : Nodejs 18"
-sudo apt install make
+sudo apt install -y make
 echo "Install successfully : Makefile"
-sudo apt install htop
-sudo snap install docker
+sudo apt install -y htop
+sudo snap install -y docker
 echo "Install successfully : Docker"
 sudo chown ubuntu:ubuntu /var/run/docker.sock
 echo "Install successfully : Run docker without sudo"
