e924fa16e4376b50ae8707c9094a38934f2393e4
Thanh Le Tuan <54875279+tuanthanh021094@users.noreply.github.com>
2023-06-20 19:04:52 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -20,3 +20,5 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo  curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker $USER
+newgrp docker
