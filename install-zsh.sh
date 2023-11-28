31b9cc1575f74fa8183aa8f720bc6567aa7e4251
Le Tuan Thanh <54875279+tuanthanh021094@users.noreply.github.com>
2023-11-28 18:49:20 +0700

    Create install-zsh.sh

---
+++ install-zsh.sh
@@ -0,0 +1,3 @@
+sudo apt-get install zsh -y
+sudo curl -L http://install.ohmyz.sh | sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
