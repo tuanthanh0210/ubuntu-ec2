648d0d42da0bf995dfaf691b307d168a2affcc8a
Justin <54875279+tuanthanh0210@users.noreply.github.com>
2024-01-03 10:46:35 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,9 +14,5 @@ echo "Install successfully : Nodejs 18"
 sudo apt install -y make
 echo "Install successfully : Makefile"
 sudo apt install -y htop
-sudo apt install -y docker.io
-echo "Install successfully : Docker"
-sudo chown ubuntu:ubuntu /var/run/docker.sock
-echo "Install successfully : Run docker without sudo"
 sudo npm i -g yarn
 echo "Install successfully : Yarn"
